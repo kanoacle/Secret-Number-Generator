86fd0b494cb422617d966827be855f66b76ff900
Kanojah <kanoacleveland@Kanojahs-MacBook-Pro.local>
2017-03-14 18:39:34 -1000

    secret number pau

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
+  //private
+  function createSecretNumber(){
+    return Math.floor(Math.random() * 1000000);
+  }
+  var secretNumber = createSecretNumber();
+  //public
+  function getSecretNumber(){
+    return secretNumber;
+  }
+  return getSecretNumber;
 };
